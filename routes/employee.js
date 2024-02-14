1a5bac17780cb5b03322f59678707e4311083e92
sri-ram-ravi <sriramravigit@gmail.com>
2024-02-14 11:55:22 +0530

    implemented a schema for employee

--- routes/employee.js
+++ routes/employee.js
@@ -1,9 +1,15 @@
 const express=require("express");
 /*----------------------------------*/
+const Employee=require("../model/employee")
+/*----------------------------------*/
 const router=express.Router();
 /*----------------------------------*/
 router.get("/",(req,res)=>{
     res.send({"message":"welcome to employee database system"})
+})
+
+router.get("/employee/new",(req,res)=>{
+    res.send({"message":"add employee"})
 })
 
 module.exports=router;
